126e6c28d62510072254d0987e0755cc2040a02d
eieiouka <eieiyukkuri@gmail.com>
2026-04-19 14:20:21 +0900

    Add shared execution counter with Firebase

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <main className="hero">
       <div className="hero-overlay">
         <section className="hero-inner">
-          <p className="mini">made by 計画性のないナノカちゃん</p>
+          <p className="mini">made by 計画性しかないナノちゃん</p>
           <h1>魔法少女ノ魔女裁判</h1>
           <p className="desc">※ネタバレ注意</p>
 
